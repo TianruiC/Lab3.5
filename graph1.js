1faff0ad31c669d4aefe374d5a2ca19057e04da8
CrazyMKing <CrazyMKing@CrazyMKing-2.local>
2019-03-05 13:43:59 -0500

    d

--- graph1.js
+++ graph1.js
@@ -1,5 +1,19 @@
 var dataP=d3.json("colordata.json");
 
+var drawChart=function(colordata)
+{
+  var width=400;
+  var height=200;
+  var svg=d3.select("svg")
+            .attr("width",width)
+            .attr("height",height);
+
+  svg.selectAll("rect")
+     .data(colordata)
+     .enter()
+     .append("rect");
+
+}
 dataP.then(function(data)
 {
 console.log("data",data);
